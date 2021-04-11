3e124b00432399dbf3b136bd32da577da4fe51e8
John Beresford <jberesford@volcaus.com>
2021-04-10 22:08:11 -0700

    init

---
+++ models.py
@@ -0,0 +1,23 @@
+"""
+This file defines the database models
+"""
+
+import datetime
+from .common import db, Field, auth
+from pydal.validators import *
+
+
+def get_user_email():
+    return auth.current_user.get('email') if auth.current_user else None
+
+def get_time():
+    return datetime.datetime.utcnow()
+
+
+### Define your table below
+#
+# db.define_table('thing', Field('name'))
+#
+## always commit your models to avoid problems later
+
+db.commit()
