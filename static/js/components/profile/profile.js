582eea91fc49bfd7e227519bb96e232ae345d334
ajzbc <contact.ajzbc@gmail.com>
2021-04-29 15:56:58 -0700

    Fixed alignment

--- static/js/components/profile/profile.js
+++ static/js/components/profile/profile.js
@@ -14,28 +14,34 @@ const profile = Vue.component("profile", {
     },
     template: `
     <main>
-        <div class="info">
-            <label for="image">
-                <input type="file" name="image" id="image" style="display:none;"/>
-                <img src="https://via.placeholder.com/150x150">
-            </label>
-            <div class="text">
-                <p class="username"> {{getUser.username}} </p>
-                <p class="status"> {{getUser.status}} </p>
-            </div>
-            <div class="buttons">
-                <button>Match History</button>
-                <button :disabled="is-Me">Challenge</button>
+        <div class="bg">
+            <div class="wrapper">
+                <div class="info">
+                    <label for="image">
+                        <input type="file" name="image" id="image" style="display:none;"/>
+                        <img src="https://via.placeholder.com/150x150">
+                    </label>
+                    <div class="text">
+                        <p class="username"> {{getUser.username}} </p>
+                        <p class="status"> {{getUser.status}} </p>
+                    </div>
+                    <div class="buttons">
+                        <button>Match History</button>
+                        <button v-if="!is-Me">Challenge</button>
+                    </div>
+                </div>
             </div>
         </div>
-        <div class="charts">
-            <div>
-                Elo Rating
-                <!-- replace with chart -->
-            </div>
-            <div>
-                Games
-                <!-- replace with chart -->
+        <div class="wrapper">
+            <div class="charts">
+                <div>
+                    Elo Rating
+                    <!-- replace with chart -->
+                </div>
+                <div>
+                    Games
+                    <!-- replace with chart -->
+                </div>
             </div>
         </div>
     </main>
