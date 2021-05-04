abda5786072add5bd2b0c56757832734a4d3d847
John Beresford <jberesford@volcaus.com>
2021-05-04 09:58:02 -0700

    added db tables

--- models.py
+++ models.py
@@ -13,6 +13,18 @@ def get_user_email():
 def get_time():
     return datetime.datetime.utcnow()
 
+db.define_table('games', [
+  Field('player_white', 'integer', 'reference auth_user', requires=IS_IN_DB(db, db.auth_user.id)),
+  Field('player_black', 'integer', 'reference auth_user', requires=IS_IN_DB(db, db.auth_user.id)),
+  Field('fen'),
+  Field('game_over', 'boolean'),
+  Field('winner', 'integer', 'reference auth_user', requires=IS_IN_DB(db, db.auth_user.id))
+])
 
+db.define_table('ratings', [
+  Field('rating', 'integer'),
+  Field('player_id', 'integer', 'reference auth_user', requires=IS_IN_DB(db, db.auth_user.id)),
+  Field('updated_on', 'datetime', requires=IS_DATETIME())
+])
 
 db.commit()
