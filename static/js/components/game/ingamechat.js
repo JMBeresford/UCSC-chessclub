bf4ab29fbad08a5f1f0ffba3564f4135ea21532b
Aaron <aswoiskin@gmail.com>
2021-05-21 10:56:34 -0700

    added basic functionality, still working on formatting

--- static/js/components/game/ingamechat.js
+++ static/js/components/game/ingamechat.js
@@ -2,13 +2,63 @@ const ingamechat = Vue.component('ingamechat', {
   props: {
     game: Object,
     players: Object,
+  },  
+  computed: {
   },
   data() {
-    return {};
+    return {
+      messageInput: String,
+      messages: Array,
+    };
+  },
+  methods:{
+    userMessage: function (id, message) {
+      this.messages.push({
+        "user_id": id,
+        "msg": message
+      })
+    },
+    sysMessage: function (message) {
+      this.messages.push({
+        "user_id": 0,
+        "msg": message
+      })
+    },
+    sendMessage: function() {
+      this.userMessage(this.players['white'].user_id, this.messageInput)
+      console.log("sending message...")
+    }
   },
   mounted: function () {},
+  created: function () {
+    this.messageInput = ""
+    this.messages = []
+  },
   template: `
-    <div id="ingamechat"></div>
+    <div id="ingamechat">
+      <div class="chatBanner">
+        <button class="btn" @click="userMessage(1, 'hi')">
+          <img src="svg/pieces/wn.svg" alt="white knight image">
+        </button>
+        <h1>{{ this.players['white'].username }} VS {{ this.players['black'].username }}</h1>
+        <button class="btn" @click="sendMessage()">
+          <img src="svg/pieces/bn.svg" alt="white knight image">
+        </button>
+      </div>
+
+      <div class="chatContainer">
+        <div v-for="m in messages" class="block">
+          <p>{{m['msg']}}</p>
+        </div>
+      </div>
+
+      <div id="messageBox">
+        <input class="input" @keyup.enter="userMessage" v-model="messageInput" placeholder="">
+        <button class="btn" @click="sendMessage()">
+          Send
+        </button>
+      </div>
+    </div>
   `,
 });
 
