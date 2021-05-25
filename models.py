68533d6a2680e8abceb576df17f4626d4a6777bb
John Beresford <jberesford@volcaus.com>
2021-05-25 09:25:39 -0700

    fixed rating table to include date

--- models.py
+++ models.py
@@ -8,33 +8,75 @@ from pydal.validators import *
 
 
 def get_user_email():
-    return auth.current_user.get('email') if auth.current_user else None
+    return auth.current_user.get("email") if auth.current_user else None
+
 
 def get_time():
-    return datetime.datetime.utcnow()
+    dt = datetime.datetime.utcnow()
+    return dt.__str__()
 
-db.define_table('games', [
-  Field('player_white', 'integer', 'reference auth_user', requires=IS_IN_DB(db, db.auth_user.id)),
-  Field('player_black', 'integer', 'reference auth_user', requires=IS_IN_DB(db, db.auth_user.id)),
-  Field('fen'),
-  Field('game_over', 'boolean'),
-  Field('winner', 'integer', 'reference auth_user', requires=IS_IN_DB(db, db.auth_user.id))
-])
 
-db.define_table('ratings', [
-  Field('rating', 'integer', default=800),
-  Field('player_id', 'integer', 'reference auth_user', requires=IS_IN_DB(db, db.auth_user.id)),
-  Field('updated_on', 'datetime', requires=IS_DATETIME())
-])
+db.define_table(
+    "games",
+    [
+        Field(
+            "player_white",
+            "integer",
+            "reference auth_user",
+            requires=IS_IN_DB(db, db.auth_user.id),
+        ),
+        Field(
+            "player_black",
+            "integer",
+            "reference auth_user",
+            requires=IS_IN_DB(db, db.auth_user.id),
+        ),
+        Field("fen"),
+        Field("game_over", "boolean"),
+        Field(
+            "winner",
+            "integer",
+            "reference auth_user",
+            requires=IS_IN_DB(db, db.auth_user.id),
+        ),
+    ],
+)
 
-db.define_table('statuses', [
-  Field('status', default=""),
-  Field('player_id', 'integer', 'reference auth_user', requires=IS_IN_DB(db, db.auth_user.id))
-])
+db.define_table(
+    "ratings",
+    [
+        Field("rating", "integer", default=800),
+        Field(
+            "player_id",
+            "integer",
+            "reference auth_user",
+            requires=IS_IN_DB(db, db.auth_user.id),
+        ),
+        Field("updated_on", default=get_time),
+    ],
+)
 
-db.define_table('profile_pictures', [
-  Field('image', 'upload'),
-  Field('player_id', 'reference auth_user', requires=IS_IN_DB(db, db.auth_user.id))
-])
+db.define_table(
+    "statuses",
+    [
+        Field("status", default=""),
+        Field(
+            "player_id",
+            "integer",
+            "reference auth_user",
+            requires=IS_IN_DB(db, db.auth_user.id),
+        ),
+    ],
+)
+
+db.define_table(
+    "profile_pictures",
+    [
+        Field("image", "upload"),
+        Field(
+            "player_id", "reference auth_user", requires=IS_IN_DB(db, db.auth_user.id)
+        ),
+    ],
+)
 
 db.commit()
