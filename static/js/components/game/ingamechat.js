e4a06e244b6fa59b165747b9bd6a351264479c3f
Aaron <aswoiskin@gmail.com>
2021-05-27 17:11:27 -0700

    chat function working

--- static/js/components/game/ingamechat.js
+++ static/js/components/game/ingamechat.js
@@ -12,22 +12,38 @@ const ingamechat = Vue.component('ingamechat', {
     };
   },
   methods:{
-    userMessage: function (id, message) {
+    addMessage: function(id, message) {
       this.messages.push({
         "user_id": id,
         "msg": message
       })
+      let el = document.querySelector(".spacer")
+      el.scrollIntoView(false, 
+        {
+          behavior: "smooth", 
+          block: "end",
+          inline: "end",
+        });
+    },
+    userMessage: function (id, message) {
+      this.addMessage(id, message)
     },
     sysMessage: function (message) {
-      this.messages.push({
-        "user_id": 0,
-        "msg": message
-      })
+      this.addMessage(0, message)
     },
     sendMessage: function() {
-      this.userMessage(this.players['white'].user_id, this.messageInput)
-      console.log("sending message...")
-    }
+      this.userMessage(this.players['white'].id, this.messageInput)
+      this.messageInput = ""
+    },
+    isWhite: function(message) {
+      return message['user_id'] == this.players['white'].id
+    },
+    isBlack: function(message) {
+      return message['user_id'] == this.players['black'].id
+    },
+    getUsername: function(color) {
+      return this.players[color].username
+    },
   },
   mounted: function () {},
   created: function () {
@@ -37,26 +53,34 @@ const ingamechat = Vue.component('ingamechat', {
   template: `
     <div id="ingamechat">
       <div class="chatBanner">
-        <button class="btn" @click="userMessage(1, 'hi')">
-          <img src="svg/pieces/wn.svg" alt="white knight image">
-        </button>
-        <h1>{{ this.players['white'].username }} VS {{ this.players['black'].username }}</h1>
-        <button class="btn" @click="sendMessage()">
-          <img src="svg/pieces/bn.svg" alt="white knight image">
-        </button>
-      </div>
-
-      <div class="chatContainer">
-        <div v-for="m in messages" class="block">
-          <p>{{m['msg']}}</p>
+        <div id="leftHorse">
+          <img src="svg/pieces/bn.svg" alt="white knight image" style="transform: scaleX(-1)" @click="userMessage(2, 'I am user 2')">
+        </div>
+        <div id="versusText">
+          <h1>{{ getUsername('white') }} VS {{ getUsername('black') }}</h1>
+          <hr>
+        </div>
+        <div id="rightHorse">
+          <img src="svg/pieces/wn.svg" alt="white knight image" @click="sysMessage('I am the server')">
         </div>
       </div>
 
+      <div class="wrapper">
+        <div class="chatContainer">
+          <div v-for="m in messages" class="chatMessage">
+            <div class="whiteMessage"  v-if="isWhite(m)">{{ getUsername('white') }}: {{m['msg']}}</div>
+            <div class="blackMessage"  v-if="isBlack(m)">{{ getUsername('black') }}: {{m['msg']}}</div>
+            <div class="serverMessage" v-if="m['user_id'] == 0">⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯ {{m['msg']}} ⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯</div>
+          </div>
+          <div class="spacer"></div>
+        </div>
+      </div>
+      
       <div id="messageBox">
-        <input class="input" @keyup.enter="userMessage" v-model="messageInput" placeholder="">
-        <button class="btn" @click="sendMessage()">
-          Send
-        </button>
+        <div class="inputWrap">
+          <input class="input" @keyup.enter="sendMessage" v-model="messageInput" placeholder="Send a message (click horses to test other user and server)">
+          <i class="fa fa-paper-plane" @click="sendMessage"></i>
+        </div>
       </div>
     </div>
   `,
